127ef4a1eab0cb3585b59dd0c1dc6e1e525a68a4
lumiere.9823 <lmt9823@gmail.com>
2024-03-30 11:52:57 +0700

    Fix Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <div>
-        Home
+
+      <div><Navigation /></div>      <div>
       </div>
     </div>
   );
--- src/customer/components/navigation/Navigation.jsx
+++ src/customer/components/navigation/Navigation.jsx
@@ -1,17 +1,3 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
 import { Fragment, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -143,7 +129,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Navigation() {
     const [open, setOpen] = useState(false)
 
     return (
